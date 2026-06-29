5c60b0fdb0bd22ce02ecd46640041ca5aa8c2157
Harry <harry.walsh303@gmail.com>
2026-06-29 14:28:05 +1200

    add tests for creating and updating a booking

--- tests/booking-test.spec.ts
+++ tests/booking-test.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test'
 import dotenv from 'dotenv'
 
+
 dotenv.config()
 let token: string
 let bookingId: number
@@ -15,7 +16,8 @@ test.beforeAll(async ({ request }) => {
     })
     const authBody = await authResponse.json()
     token = authBody.token
-
+})
+test('Should create a new booking', async ({request})=> {
     const bookingResponse = await request.post('/booking', {
         headers: {
             'Content-Type': 'application/json',
@@ -36,16 +38,40 @@ test.beforeAll(async ({ request }) => {
     const bookingBody = await bookingResponse.json()
     bookingId = bookingBody.bookingid
 })
-test('Should return all bookings', async ({request}) =>
+test('Should return all bookingids', async ({request}) =>
 {
     const response = await request.get('/booking')
     const responseBody = await response.json()
     expect(responseBody[0]).toHaveProperty('bookingid')
+    expect(responseBody[5]).toHaveProperty('bookingid')
 })
-test('should return the details for a specific booking', async ({request}) =>
+test('Should return the details for a specific booking', async ({request}) =>
 {
     const response = await request.get(`/booking/${bookingId}`)
     const responseBody = await response.json()
     expect(responseBody).toHaveProperty('firstname')
     expect(responseBody.lastname).toBe('Bezos')
+})
+test('Should update the details of a specific booking', async ({request}) => {
+    const response = await request.put(`/booking/${bookingId}`,{
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json',
+            'Cookie': `token=${token}`
+        },
+        data:{
+            'firstname':'Jeffrey',
+            'lastname':'Bezos',
+            'totalprice':1000000000000,
+            'depositpaid':true,
+            'bookingdates': {
+                'checkin': '2020-06-06',
+                'checkout': '2026-12-12'
+            },
+            'additionalneeds':'A monopoly over American consumer products'
+        }
+    })
+    const responseBody = await response.json()
+    expect(responseBody.firstname).toBe('Jeffrey')
+    expect(responseBody.additionalneeds).toBe('A monopoly over American consumer products')
 })
